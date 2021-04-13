b5fc07ee650510956d29e1f73026ed1c5830511d
Santo Blasius <sntblasius@gmail.com>
2021-04-13 15:22:03 +0700

    Setting up jwt auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['api'])->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    });
 });
